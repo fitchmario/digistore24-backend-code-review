d8043af7a55b74dce61aa46e1470517973e51969
mariofitch <mario.filipovic.dev@gmail.com>
2025-09-25 14:41:58 +0200

    updating docs

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -17,8 +17,16 @@ use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 
 /**
  * @see MessageControllerTest
- * TODO: review both methods and also the `openapi.yaml` specification
- *       Add Comments for your Code-Review, so that the developer can understand why changes are needed.
+ *
+ * Previous solution was very bad and there multiple reasons:
+ * - REST api wasn't good, it was GET /messages/send for send() method. GET can't be used for creating and URI should be as same as for GET
+ * - GET was missing HTTP method setup, so after fixing creating method send() and switching it to POST, conflict would be created
+ * - method list() used directly repositories and builders in controllers as well as setting JSON response with config in Response class
+ * - message repository method name by() is very bad, it's not self-explanatory and could cause miss understanding
+ * - method send() does writing to DB, and it's a command in CQRS so it should be async but should return uuid property in order for FE to catch the last message for it UI/UX
+ * - for the POST HTTP method, it's bad to send data by Query params, it should be done in payload
+ *
+ * All changes here are update in openapi.yml file
  */
 class MessageController extends AbstractController
 {
